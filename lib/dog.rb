19050261be0df9076f2eeb79cb8fd44d2225a0bd
Mark May <mamark90@gmail.com>
2019-06-22 13:32:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Dog
 
   attr_accessor :mood, :owner
-  attr_reader
+  attr_reader :name
 
   def initialize(name, mood = "")
     @name = name
@@ -11,7 +11,7 @@ class Dog
   end
 
   def name
-    @name.freeze
+    @name
   end
 
 end
